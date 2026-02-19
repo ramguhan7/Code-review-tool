1b2f2fe0a08b6a534524a901dfaacd6013c5dd25
RamGuhan Gandhi <ramguhan.gandhi@healthcatalyst.com>
2026-02-19 09:18:01 -0500

    entered bad comment

--- demo_sql_files/bad aliases.sql
+++ demo_sql_files/bad aliases.sql
@@ -1,7 +1,7 @@
 SELECT
     order_id,
     amount as amt,
-    NULL AS Dummycolumn
+    NULL AS Dummycolumn --why this
 FROM orders o
 JOIN customers c ON o.customer_id = c.id --join needed
-JOIN patients p on o.patienid = p.id
+JOIN patients p on o.patienid = p.id 
