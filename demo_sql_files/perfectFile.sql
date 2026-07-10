4be21b9e050aee47ea09944e9d27ad87b3043ca6
RamGuhan Gandhi <ramguhan.gandhi@healthcatalyst.com>
2026-07-09 21:40:28 -0400

    good query update

--- demo_sql_files/perfectFile.sql
+++ demo_sql_files/perfectFile.sql
@@ -91,5 +91,5 @@ FROM
         ON pop.DIS_ACCT_UNIT = d.ACCT_UNIT
         AND pop.COMPANY = d.COMPANY
     LEFT JOIN shca_source_data.lawson.acactivity AS a
-        ON pop.ACTIVITY = a.activity --dont need this join 
+        ON pop.ACTIVITY = a.activity /*dont need this join*/
 ;
