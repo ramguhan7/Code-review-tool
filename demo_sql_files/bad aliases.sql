9631ae58b836c5d2b22c9f16290ef65d1db153ee
RamGuhan Gandhi <ramguhan.gandhi@healthcatalyst.com>
2026-07-16 13:55:24 -0400

    bad commit

--- demo_sql_files/bad aliases.sql
+++ demo_sql_files/bad aliases.sql
@@ -3,5 +3,5 @@ select
     amount,
     NULL AS Dummycolumn --why this
 from orders o
-JOIN customers c ON o.customer_id = c.id --join needed
-JOIN patients p on o.patienid = p.id -- no join needed
+join customers c ON o.customer_id = c.id --join needed
+JOIN patients p on o.patienid = p.id -- no join neededs
